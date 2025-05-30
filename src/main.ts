7f1cfb3553a3013b71ecb171987ce5da4a8ee96e
Mohamad Supangat <moha.supangat@gmail.com>
2025-05-31 04:06:25 +0700

    testing

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,35 @@ import kodepos from "./controllers/kodepos";
 import sekolah from "./controllers/sekolah";
 const port = process.env.PORT || 3000;
 
-serve({
-  idleTimeout: 100,
-  port,
-  development: true,
-  async fetch(req) {
-    const currentUrl = new URL(req.url);
-    let searchQuery: string =
-      currentUrl.searchParams.get("q")?.toString() ?? "";
-    searchQuery = realEscapeString(searchQuery)!;
+export default function handler(req: Request) {
+  return new Response(
+    JSON.stringify({ message: `Hello from bun@${Bun.version}` }),
+    {
+      headers: { "Content-Type": "application/json" },
+    },
+  );
+}
 
-    if (!isEmpty(searchQuery) && currentUrl.pathname == "/kodepos/search") {
-      return kodepos(searchQuery);
-    }
-
-    if (!isEmpty(searchQuery) && currentUrl.pathname == "/sekolah/search") {
-      return sekolah(searchQuery);
-    }
-
-    return Response.redirect("https://s.id/aurakomputer");
-  },
-});
-
-console.log(`listening on ${port}`);
+// serve({
+//   idleTimeout: 100,
+//   port,
+//   development: true,
+//   async fetch(req) {
+//     const currentUrl = new URL(req.url);
+//     let searchQuery: string =
+//       currentUrl.searchParams.get("q")?.toString() ?? "";
+//     searchQuery = realEscapeString(searchQuery)!;
+//
+//     if (!isEmpty(searchQuery) && currentUrl.pathname == "/kodepos/search") {
+//       return kodepos(searchQuery);
+//     }
+//
+//     if (!isEmpty(searchQuery) && currentUrl.pathname == "/sekolah/search") {
+//       return sekolah(searchQuery);
+//     }
+//
+//     return Response.redirect("https://s.id/aurakomputer");
+//   },
+// });
+//
+// console.log(`listening on ${port}`);
